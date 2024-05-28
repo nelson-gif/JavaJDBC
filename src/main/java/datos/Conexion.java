27df1922fa3265b10da495f6a875a0054cebcfd5
nelson-gif <nelsonvelasquez2.nv@gmail.com>
2024-05-28 02:14:45 -0600

    agregando pool de conexiones

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -2,14 +2,29 @@ package datos;
 
 import java.sql.*;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Conexion {
 
 	private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 	private static final String JDBC_USER = "root";
 	private static final String JDBC_PASSWORD = "admin";
 	
+	public static DataSource getDataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setUrl(JDBC_URL);
+		ds.setUsername(JDBC_USER);
+		ds.setPassword(JDBC_PASSWORD);
+		
+		//definiendo tamanio inicial del pool de conexiones
+		ds.setInitialSize(5); //inicializando 5 conexiones
+		return ds;
+	}
+	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+		return getDataSource().getConnection();
 	}
 	
 	public static void close(ResultSet rs) throws SQLException {
